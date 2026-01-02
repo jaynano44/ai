638309e8fe6354e0b0fd7cd6f42067eabec34366
jaynano44 <lostland4444@gmai.com>
2026-01-02 10:38:56 +0900

    프로필 적용

---
+++ source/11_flask/ch6_todo/database/connection.py
@@ -0,0 +1,13 @@
+# pip install python-dotenv
+import cx_Oracle
+import os
+from dotenv import load_dotenv
+# conn =cx_Oracle.connect(user="scott", password="tiger", dsn="127.0.0.1:1521/xe")
+load_dotenv()
+dbserver_ip = os.getenv('DBSERVER_IP')
+oracle_port = os.getenv('ORACLE_PORT')
+oracle_user = os.getenv('ORACLE_USER')
+oracle_password = os.getenv('ORACLE_PASSWORD')
+conn = cx_Oracle.connect(oracle_user, 
+                  oracle_password,
+                  f"{dbserver_ip}:{oracle_port}/xe")
